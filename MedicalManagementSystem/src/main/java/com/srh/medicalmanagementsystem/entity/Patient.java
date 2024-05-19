4e7e8ddb91485e3745dfc5f44dcb136a7a0d1fbb
DevJLALW <115839414+DevJLALW@users.noreply.github.com>
2024-05-19 19:48:50 +0200

    Spring Boot code for Patient table

---
+++ MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/entity/Patient.java
@@ -0,0 +1,40 @@
+package com.srh.medicalmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Patients")
+@Data
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PatientID")
+    private Integer patientId;
+
+    @Column(name = "FirstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "LastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "DOB", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+
+    @Column(name = "Gender", nullable = false)
+    private String gender;
+
+    @Column(name = "ContactNumber", nullable = false)
+    private String contactNumber;
+
+    @Column(name = "Email", nullable = false)
+    private String email;
+
+    @Column(name = "Address", nullable = false)
+    private String address;
+}
+
