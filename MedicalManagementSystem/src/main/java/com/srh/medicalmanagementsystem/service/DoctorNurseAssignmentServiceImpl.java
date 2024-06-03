b95570fce604c78cf6932b46e8a9cc299c5bcbec
Neha-Jacob <neha.k.jacob@gmail.com>
2024-06-03 15:00:37 +0200

    Updated DoctorNurseAssignment Service

--- MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/service/DoctorNurseAssignmentServiceImpl.java
+++ MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/service/DoctorNurseAssignmentServiceImpl.java
@@ -1,4 +1,53 @@
 package com.srh.medicalmanagementsystem.service;
 
-public class DoctorNurseAssignmentServiceImpl {
+import com.srh.medicalmanagementsystem.dao.DoctorNurseAssignmentRepository;
+import com.srh.medicalmanagementsystem.entity.DoctorNurseAssignment;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class DoctorNurseAssignmentServiceImpl implements DoctorNurseAssignmentService{
+
+    private DoctorNurseAssignmentRepository assignmentRepository;
+
+    @Autowired
+    public DoctorNurseAssignmentServiceImpl(DoctorNurseAssignmentRepository assignmentRepository) {
+        this.assignmentRepository = assignmentRepository;
+    }
+
+    @Override
+    public List<DoctorNurseAssignment> getAllDoctorNurseAssignments() {
+        List<DoctorNurseAssignment> assignments = assignmentRepository.findAll();
+        assignments.sort(Comparator.comparing(
+                DoctorNurseAssignment::getAssignmentId
+        ).reversed());
+        return assignments;
+    }
+
+    @Transactional
+    public DoctorNurseAssignment saveDoctorNurseAssignment (DoctorNurseAssignment assignment) {
+        return assignmentRepository.save(assignment);
+    }
+
+    @Override
+    public boolean deleteDoctorNurseAssignments (List<Integer> assignmentIds) {
+        boolean allDeleted = true;
+        for (Integer assignmentId: assignmentIds) {
+            if (assignmentRepository.existsById(assignmentId)) {
+                assignmentRepository.deleteById(assignmentId);
+            } else {
+                allDeleted = false;
+            }
+        }
+        return allDeleted;
+    }
+
+    public List<DoctorNurseAssignment> findAssignmentsByIds (Integer employeeId) {
+        return assignmentRepository.searchAssignments(employeeId);
+    }
+
 }
