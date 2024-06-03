463a2d26d9a09c0565597d2ac2cee1abf081fa48
Neha-Jacob <neha.k.jacob@gmail.com>
2024-06-03 14:13:29 +0200

    Updated CreateEmployee

--- MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/controller/EmployeeControllerPages.java
+++ MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/controller/EmployeeControllerPages.java
@@ -1,4 +1,85 @@
 package com.srh.medicalmanagementsystem.controller;
 
+import com.srh.medicalmanagementsystem.entity.Employee;
+import com.srh.medicalmanagementsystem.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeControllerPages {
+
+    private EmployeeService employeeService;
+
+    public EmployeeControllerPages(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public String getAllEmployeesPage(Model model) {
+        List<Employee> employeeList= employeeService.getAllEmployees();
+        model.addAttribute("employees",employeeList);
+
+        return "patients/ShowEmployees";
+    }
+
+    @GetMapping("/create")
+    public String showCreateEmployeePage(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employees/CreateEmployee";
+    }
+
+    @PostMapping("/create")
+    public String createEmployee(
+            @Valid @ModelAttribute("employee") Employee employee,
+            BindingResult bindingResult, Model model
+    ) {
+        if(bindingResult.hasErrors()){
+            return "employees/CreateEmployee";
+        }
+        employeeService.saveEmployee(employee);
+        return "redirect:/employees/all";
+    }
+
+    @GetMapping("/update/{employeeId}")
+    public String showUpdateEmployeePage(
+            @PathVariable("employeeId") Integer employeeId,
+            Model model
+    ) {
+        Employee employee= employeeService.findEmployeeById(employeeId);
+        model.addAttribute("employee", employee);
+        return "employees/UpdateEmployee";
+    }
+
+    @PostMapping("/update/{employeeId}")
+    public String updateEmployee(
+            @PathVariable("employeeId") Integer employeeId,
+            @ModelAttribute("employee") Employee employee
+    ) {
+        employeeService.updateEmployee(employeeId, employee);
+        return "redirect:/employees/all";
+    }
+
+    @PostMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeIds") List<Integer> employeeIds) {
+        employeeService.deleteEmployees(employeeIds);
+
+        return "redirect:/employees/all";
+    }
+
+    @GetMapping("/search")
+    public String searchEmployees(
+            @RequestParam("keyword") String keyword,
+            Model model
+    ) {
+        List<Employee> employeeList =employeeService.searchEmployees(keyword);
+        model.addAttribute("employees", employeeList);
+
+        return "employees/ShowEmployees";
+    }
 }
