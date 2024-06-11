aa977b78eedb1a0774e3815062a7bd1f44b04e6c
Neha-Jacob <neha.k.jacob@gmail.com>
2024-06-11 15:41:31 +0200

    Removed Assignment

--- MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/dao/AssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.srh.medicalmanagementsystem.dao;
-
-import com.srh.medicalmanagementsystem.entity.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
-}
-
