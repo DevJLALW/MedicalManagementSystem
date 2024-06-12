b594fd543435e8e2a32b096b68d49a5b048c8ba0
dhool19 <ayeshadhool24@gmail.com>
2024-06-12 19:29:23 +0200

    Deleted extra files (3)

--- MedicalManagementSystem/src/main/java/com/srh/medicalmanagementsystem/dao/CallRepository.java
+++
@@ -1,8 +0,0 @@
-package com.srh.medicalmanagementsystem.dao;
-
-import com.srh.medicalmanagementsystem.entity.Call;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CallRepository extends JpaRepository<Call, Integer> {
-}
-
